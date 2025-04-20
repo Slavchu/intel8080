675dc70af98f227931985d8e2a9868f825815b41
Slavchu <tothelove1@gmail.com>
2025-04-20 23:35:40 +0300

    debug: added debug macrosses

--- include/common.hpp
+++ include/common.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <stdint.h>
-
-#define MEMORY_SIZE 65536
-#define FREQUENCY_HZ 2000
+#include <iostream>
 
 // emulator defaults
+#define MEMORY_SIZE 65536
+#define FREQUENCY_HZ 2000
 #define DEFAULT_PROGRAM_PATH "program.bin"
+
+// macrosses
+#define DEBUG_ERROR(err_msg) std::cerr << "ERROR:" <<  __FILE__  << " (" << __PRETTY_FUNCTION__ << ":" << __LINE__ << "):\t\t" err_msg << std::endl 
